4af8c9dd36f227645bd44c01d408e715deafb7e0
Ruudd <secretguest@live.nl>
2016-01-06 15:38:22 +0100

    Dbcon commit 0.1.1.1
    i tried

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -4,9 +4,15 @@
  */
 
 use Core\Language;
-
 ?>
-
+	
 	<div class="page-header">
 		<h1>Login</h1>
 	</div>
+	<form action="submit" method="post">
+
+	<p>Username<br /><input type='text' name='username'></p>
+	<p>Password<br /><input type='password' name='password'></p>
+	<p><input type='submit' name='submit' value='Login'></p>
+
+	</form>
