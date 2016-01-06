b59ae7bf944148cdb8f0ab454f044aa669cdbf6d
Chris_GSPvP <christiaanbruijnes@hotmail.com>
2016-01-06 10:17:25 +0100

    Footer aangemaakt en basis CSS is er.

--- app/templates/default/footer.php
+++ app/templates/default/footer.php
@@ -13,6 +13,33 @@ $hooks = Hooks::get();
 
 </div>
 
+
+<div id="footer">
+		
+		<div class="top-footer">
+			<div class="container">
+				<span class="title">Heeft u zich al ingeschreven voor een cursus? Schrijf u nu in!</span>
+			</div>
+		</div>
+		<div class="container">
+			<div class="col-md-4">
+				<h2>Over het bedrijf</h2>
+			</div>
+			<div class="col-md-4">
+				<h2>Links</h2>
+			</div>
+			<div class="col-md-4">
+				<h2>Adres gegevens</h2>
+			</div>
+		</div>
+
+
+</div>
+
+
+
+
+
 <!-- JS -->
 <?php
 Assets::js(array(
