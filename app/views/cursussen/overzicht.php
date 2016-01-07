9d79b4185180d9294b7cc62b42631eba6d21d6c4
Aukevanoost <auke.v.o@hotmail.com>
2016-01-07 15:32:25 +0100

    Registreren overzichtelijk gemaakt

--- app/views/cursussen/overzicht.php
+++ app/views/cursussen/overzicht.php
@@ -15,21 +15,17 @@
             <img src="/zeilschooldewaai/app/templates/default/img/boten/boot1.png" />
             <h3>Beginners cursus</h3>
             <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-                penatibus et magnis dis parturient.
+                De cursus zal starten in Week 2, 2016. in deze cursus wordt de basis van zeilen uitgelegd. deze cursus is geschikt voor de leeftijd 6 tot 80 jaar.
             </p>
-            <div><a href="">Inschrijven</a> | <a href="#agenda"  onclick="ChangeContent('beginners')">Meer info</a></div>
+            <div><a href="#agenda"  onclick="ChangeContent('beginners')">Meer info</a></div>
         </div>
         <div class="AgendaItem">
             <img src="/zeilschooldewaai/app/templates/default/img/boten/boot2.jpg" />
             <h3>Wadtocht</h3>
             <p>
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-                penatibus et magnis dis parturient.
+                Altijd als eens over de waddenzee willen zeilen? dat kan nu. laat u meenemen over de waddenzee voor een week. (deze cursus is geldig in week 3)
             </p>
-            <div><a href="">Inschrijven</a> | <a  href="#agenda"  onclick="ChangeContent('wadtochten')">Meer info</a></div>
+            <div><a  href="#agenda"  onclick="ChangeContent('wadtochten')">Meer info</a></div>
         </div>
         <div class="AgendaItem">
             <img src="/zeilschooldewaai/app/templates/default/img/boten/boot3.jpg" />
@@ -39,7 +35,7 @@
                 Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
                 penatibus et magnis dis parturient.
             </p>
-            <div><a href="">Inschrijven</a> | <a  href="#agenda"  onclick="ChangeContent('beginners')">Meer info</a></div>
+            <div><a  href="#agenda"  onclick="ChangeContent('beginners')">Meer info</a></div>
         </div>
         <div class="AgendaItem">
             <img src="/zeilschooldewaai/app/templates/default/img/boten/boot1.png" />
@@ -49,7 +45,7 @@
                 Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
                 penatibus et magnis dis parturient.
             </p>
-            <div><a href="">Inschrijven</a> | <a  href="#agenda"  onclick="ChangeContent('ervaren')">Meer info</a></div>
+            <div><a  href="#agenda"  onclick="ChangeContent('ervaren')">Meer info</a></div>
         </div>
         <div class="AgendaItem">
             <img src="/zeilschooldewaai/app/templates/default/img/boten/boot4.jpg" />
@@ -59,7 +55,7 @@
                 Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
                 penatibus et magnis dis parturient.
             </p>
-            <div><a href="">Inschrijven</a> | <a  href="#agenda"  onclick="ChangeContent('wadtochten')">Meer info</a></div>
+            <div><a  href="#agenda"  onclick="ChangeContent('wadtochten')">Meer info</a></div>
         </div>
         <div class="AgendaItem">
             <img src="/zeilschooldewaai/app/templates/default/img/boten/boot5.jpg" />
@@ -69,7 +65,7 @@
                 Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque
                 penatibus et magnis dis parturient.
             </p>
-            <div><a href="">Inschrijven</a> | <a  href="#agenda"  onclick="ChangeContent('beginners')">Meer info</a></div>
+            <div><a  href="#agenda"  onclick="ChangeContent('beginners')">Meer info</a></div>
         </div>
         <div class="spacer" style="clear: both;"></div>
 </div>
