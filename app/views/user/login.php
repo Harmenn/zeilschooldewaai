41c7e66daf87a5ca1cb005c63d34d625d9871ca1
Ruudd <secretguest@live.nl>
2016-01-07 14:37:20 +0100

    bugfix

--- app/views/user/login.php
+++ app/views/user/login.php
@@ -20,6 +20,7 @@ else{
             <h1>Login</h1>
     </div>
         <form action="" method="post" class="col-xs-3">
+        <?php echo $data["error"]; ?>
             <p>E-mail<br /><input class="form-control" type='text' name='username'></p>
             <p>Wachtwoord<br /><input class="form-control" type='password' name='password'></p>
             <p style="float: right"><input class="btn btn-info" type='submit' name='submit' value='Login'></p>
