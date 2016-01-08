0d0668ea8399e90acc0d3270aa0ff7197e48fee9
Chris_GSPvP <christiaanbruijnes@hotmail.com>
2016-01-08 10:20:35 +0100

    Profiel

--- app/Controllers/Profiel.php
+++ app/Controllers/Profiel.php
@@ -1,41 +1,32 @@
 <?php
-/**
- * Welcome controller
- *
- * @author David Carr - dave@daveismyname.com
- * @version 2.2
- * @date June 27, 2014
- * @date updated Sept 19, 2015
- */
 
 namespace Controllers;
 
 use Core\View;
 use Core\Controller;
 use Core\Config;
-
 /**
  * Sample controller showing a construct and 2 methods and their typical usage.
  */
 class Profiel extends Controller
 {
-
     /**
      * Call the parent construct
      */
-
+    private $profiel;
     public function __construct()
     {
         parent::__construct();
-        //$this->registreren = new \Models\Db();
+        $this->profiel = new \Models\Db();
     }
 
     public function index()
     {
-        $data['title'] = $this->language->get('Profiel');
-        $data['home_message'] = $this->language->get('home_message');
-        
-        
+
+        $data['title'] = "Profiel";
+        $email = \Helpers\Session::get('username');
+        $data['klant'] = $this->profiel->getUser($email);
+          
         View::renderTemplate('header', $data);
         View::render('user/profiel', $data);
         View::renderTemplate('footer', $data);
--- app/views/user/profiel.php
+++ app/views/user/profiel.php
@@ -8,8 +8,16 @@ if (\Helpers\Session::get('username')) {
 	?>
 		<div class="page-header">
 		        <h1>Profiel</h1>
+		        
 		</div>
 	<?php
+	var_dump($data['klant']);
+	echo '<br><br><br>';
+	foreach($data['klant'] as $key => $value){
+		echo $key[0];
+	}
+
+
 }
 else{
 	\Helpers\Url::redirect('home');
