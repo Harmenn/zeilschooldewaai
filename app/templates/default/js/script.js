c76f17881810ee12e0bc8defc182bb42cfa3b92c
Aukevanoost <auke.v.o@hotmail.com>
2016-01-08 11:40:32 +0100

    Voortgang inschrijven

--- app/templates/default/js/script.js
+++ app/templates/default/js/script.js
@@ -25,3 +25,23 @@ function ChangeContent(content){
     $(".Subject").slideUp( "500", function() {  });
     $("#" + content).slideDown( "500", function() {  });
 }
+
+$(".CursusRij").click(function() {
+    $(".SelectArrow").removeClass('active');
+    var cursusId = $(this).attr('data-cursusid');
+    $('#cursus' + cursusId).prop('checked', true);
+    $("#CursusIcon" + cursusId).addClass('active');
+    $("#stap2").removeClass('disabled');
+    //CursusIcon
+});
+
+$(".btnstap").click(function() {
+    if ( $( this ).hasClass( "disabled" ) ) {
+        console.log('test');
+    }else{
+        var stap = $(this).attr('id');
+        console.log(stap);
+        $(".Subject").slideUp( "500", function() {  });
+        $("#" + stap + "_inschrijven").slideDown( "500", function() {  });
+    }
+});
--- app/views/cursussen/overzicht.php
+++ app/views/cursussen/overzicht.php
@@ -1,13 +1,16 @@
 <br />
-<!-- Nav tabs -->
+<!-- Navigatie
+----------------------------------------------------------------------------- -->
 <ul class="nav nav-tabs" role="tablist">
     <li role="presentation" class="active"><a href="#agenda" onclick="ChangeContent('Agenda')" role="tab" data-toggle="tab">Deze maand</a></li>
     <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('beginners')" data-toggle="tab">Beginners cursus</a></li>
     <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('ervaren')" data-toggle="tab">Ervaren cursus</a></li>
     <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('wadtochten')" data-toggle="tab">Wadtochten</a></li>
-    <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('inschrijven')" data-toggle="tab">Inschrijven</a></li>
+    <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('stap1_inschrijven')" data-toggle="tab">Inschrijven</a></li>
 </ul>
 
+<!-- Wat er deze maand laten zien wordt.
+----------------------------------------------------------------------------- -->
 <div id="Agenda" class="Subject">
     <div class="page-header">
         <h1 style="text-align: center;">In de planning</h1>
@@ -71,6 +74,8 @@
         <div class="spacer" style="clear: both;"></div>
 </div>
 
+<!-- Uitleg beginners cursus
+----------------------------------------------------------------------------- -->
 <div id="beginners" class="Subject" style="display: none;">
     <div class="col-md-7">
         <div class="page-header">
@@ -100,6 +105,8 @@
     <br />
 </div>
 
+<!-- Uitleg Ervaren cursus
+----------------------------------------------------------------------------- -->
 <div id="ervaren" class="Subject" style="display: none;">
     <div class="col-md-7">
         <div class="page-header">
@@ -129,6 +136,8 @@
     <br />
 </div>
 
+<!-- uitleg Wadtochten
+----------------------------------------------------------------------------- -->
 <div id="wadtochten" class="Subject" style="display: none">
     <div class="col-md-7">
         <div class="page-header">
@@ -159,22 +168,93 @@
 </div>
 
 
+<!-- Stap 1 Inschrijvingen
+----------------------------------------------------------------------------- -->
+<div id="stap1_inschrijven" class="Subject" style="display: none">
+    <div class="page-header">
+        <h1>Kies een cursus</h1>
+    </div>
+    <?php if (\Helpers\Session::get('id')) { ?>
+       <button class="btn btn-success btnstap" id="stap1">Stap 1</button> <button id="stap2" class="btnstap btn btn-default disabled">Stap 2</button> <button id="stap3" class="btnstap btn btn-default disabled">Stap 3</button> <button id="stap4" class="btnstap btn btn-default disabled">Stap 4</button>
+        <table class="table table-hover"  id="CursusTable">
+            <thead>
+                <tr>
+                    <th width="40px">#</th>
+                    <th>Naam cursus</th>
+                    <th width="150px">Begindatum</th>
+                    <th width="150px">Einddatum</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr data-cursusid="1" class="CursusRij">
+                    <td><input type="radio" id="cursus1" name="cursus" value="1" /><div class="SelectArrow" id="CursusIcon1" ></div></td>
+                    <td>Beginnerscursus voor Beginners</td>
+                    <td>10 januari 2016</td>
+                    <td>15 januari 2016</td>
+                </tr>
+                <tr data-cursusid="2" class="CursusRij">
+                    <td><input type="radio" id="cursus2" name="cursus" value="2" /><div class="SelectArrow" id="CursusIcon2" ></div></td>
+                    <td>Waddentocht</td>
+                    <td>21 februari 2016</td>
+                    <td>26 februari 2016</td>
+                </tr>
+                <tr data-cursusid="3" class="CursusRij">
+                    <td><input type="radio" id="cursus3" name="cursus" value="3" /><div class="SelectArrow" id="CursusIcon3" ></div></td>
+                    <td>Ervaren cursus</td>
+                    <td>13 maart 2016</td>
+                    <td>18 maart 2016</td>
+                </tr>
+                <tr data-cursusid="4" class="CursusRij">
+                    <td><input type="radio" id="cursus4" name="cursus" value="4" /><div class="SelectArrow" id="CursusIcon4" ></div></td>
+                    <td>Zomertocht</td>
+                    <td>04 April 2016</td>
+                    <td>10 April 2016</td>
+                </tr>
 
-<div id="inschrijven" class="Subject" style="display: none">
-    <div class="col-md-12">
-        <div class="page-header">
-            <h1>Inschrijven</h1>
-        </div>
-        <?php
-            if (\Helpers\Session::get('username')) {
-                echo "Formulier hier, persoon is ingelogd.";
-            }
-            else{
-                echo "Maak eerst een account aan of log-in voordat je je kunt inschrijven.";
-            }
-        ?>
+            </tbody>
+        </table>
+    <?php } else{ ?>
+        Maak eerst een account aan of log-in voordat je je kunt inschrijven.
+    <?php } ?>
+</div>
+<div id="stap2_inschrijven" class="Subject" style="display: none">
+    <div class="page-header">
+        <h1>Benodigde informatie</h1>
+    </div>
+    <button class="btn btn-default btnstap" id="stap1">Stap 1</button> <button id="stap2" class="btnstap btn btn-success">Stap 2</button> <button id="stap3" class="btnstap btn btn-default">Stap 3</button> <button id="stap4" class="btnstap btn btn-default disabled">Stap 4</button>
+    <br />
+    <div class="col-md-7">
+        <br /><br />
+        <p>
+            uiteraard hebben wij uw gegevens al waardoor u alleen<br /> nog maar uw voorkeur, vragen en/of opmerkingen hoeft in te vullen.
+        </p>
+        <br />
+        <label>Opmerkingen / Vragen / Voorkeur</label>
+        <textarea class="form-control" id="Comments" name="comments"></textarea>
     </div>
     <div class="spacer" style="clear: both;"></div>
     <br />
     <br />
+
+</div>
+<div id="stap3_inschrijven" class="Subject" style="display: none">
+    <div class="page-header">
+        <h1>Voorwaarden</h1>
+    </div>
+    <button class="btn btn-default btnstap" id="stap1">Stap 1</button> <button id="stap2" class="btnstap btn btn-default">Stap 2</button> <button id="stap3" class="btnstap btn btn-success">Stap 3</button> <button id="stap4" class="btnstap btn btn-default disabled">Stap 4</button>
+    <br />
+    <div class="col-md-7">
+        <br /><br />
+        <p>
+            Dit is de laatste stap, Als u op Schrijf in klikt<br /> wordt u ingeschreven in de aangevraagde cursus.
+        </p>
+        <br />
+        <input type="checkbox" alt="Ik accepteer de voorwaarden" name="voorwaarden" />
+        <label>Ik accepteer de <a href="">Voorwaarden</a> </label><br />
+        <input type="submit" class="btn btn-primary" name="Verstuur" value="Inschrijven"/>
+    </div>
+    <div class="spacer" style="clear: both;"></div>
+    <br />
+    <br />
+
 </div>
