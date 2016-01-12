ea913152675caa00fbcdd0aa51b5073d78dcbccb
Neytoxic <Lucvden@hotmail.com>
2016-01-12 09:43:01 +0100

    Beheerder

---
+++ app/views/beheer/beheer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Sample layout
+ */
+
+use Core\Language;
+
+?>
+
+
+<br />
+<!-- Nav tabs -->
+<ul class="nav nav-tabs" role="tablist">
+    <li role="presentation" class="active"><a href="#OnzeVloot" onclick="ChangeContent('OnzeVloot')" role="tab" data-toggle="tab">Beheer Profiel</a></li>
+    <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('BootTypes')" data-toggle="tab">Beheer Klanten</a></li>
+    <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('BootTypes')" data-toggle="tab">Beheer Boten</a></li>
+    <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('BootTypes')" data-toggle="tab">Beheer Cursussen</a></li>
+    <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('BootTypes')" data-toggle="tab">Beheer Instructeurs</a></li>
+    <li role="presentation"><a href="#Soorten" role="tab"  onclick="ChangeContent('BootTypes')" data-toggle="tab"></a>Cursist Koppelen</li>
+</ul>
